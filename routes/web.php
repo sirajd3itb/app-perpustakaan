f94216f021bc3a4ed30b7590386e71349410afbe
sirajd3itb <sirajd3itb@gmail.com>
2026-09-02 04:30:06 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Resource routes
+Route::resource('books', BookController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
+Route::resource('members', MemberController::class);
+Route::resource('loans', LoanController::class);
+
+// Custom route aksi pengembalian
+Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
+    ->name('loans.kembalikan');
